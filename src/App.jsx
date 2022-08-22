7e540936c63290bceb883c513a5edcea5d2c2ba7
Rajat020730 <Rajat020730@gmail.com>
2022-08-22 18:32:08 +0530

    init

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default () => (
+  <React.Fragment>
+    <h1>Welcome to React Vite Micro App!</h1>
+    <p>Hard to get more minimal than this React app.</p>
+    <p>This app is made by me</p>
+    oh really I can write anything in This
+  </React.Fragment>
+);
